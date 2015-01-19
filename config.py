b3550604a57ec5c26c9c0f1a45c69407add493bc
Nick Barrett <nick@oxygem.com>
2015-01-19 21:37:17 +0000

    Updated & simplified config

--- config.py
+++ config.py
@@ -6,39 +6,12 @@ from .views.public import status
 from .views.dashboard import dashboard
 
 # Basics
-NAME = 'Network'
+TITLE = 'Network'
 ICON = 'globe'
 COLOR = 'purple'
 
-# Routing
+# Module routes
 ROUTES = [
     ('', ['GET'], dashboard),
     ('/status', ['GET'], status)
 ]
-
-# Objects
-OBJECTS = {
-    'device': [
-        ('device', 'Device'),
-        ('device_group', 'DeviceGroup')
-    ],
-    'ipblock': [
-        ('ipblock', 'IpBlock')
-    ]
-}
-
-# Node tasks
-TASKS = {
-    'device': {
-        'automate': 'automate.js',
-        'check': 'check.js',
-        'monitor': 'monitor.js'
-    }
-}
-
-# Node websockets
-WEBSOCKETS = {
-    'device': {
-        'console': 'websockets/console.js'
-    }
-}
