e377c8deeea33e62045c3f61a62603e80a2fb312
Nick Barrett <nick@oxygem.com>
2015-01-19 21:37:01 +0000

    preload devices

--- __init__.py
+++ __init__.py
@@ -1,11 +1,34 @@
-from util import log
+# Oxypanel Network
+# File: __init__.py
+# Desc: load up initial bits for the network module
+
+from os import path, listdir
+from glob import glob
+from importlib import import_module
+
+from util.log import logger
 
 from . import config
 
 
+# Load up device configs
 def load():
-    log.debug('  Loading device configs...')
-    config.DEVICES = ['test']
+    logger.debug('  Loading device configs...')
+    files = glob(path.join('modules', 'network', 'devices', '*.py'))
+    devices = {}
+    scripts = {}
 
-    log.debug('  Loading virtual device configs...')
-    config.VIRTUAL_DEVICES = ['whater']
+    for file in files:
+        if file.endswith('__.py'): continue
+
+        # Load the config
+        file = path.basename(file).replace('.py', '')
+        logger.debug('  + {0}'.format(file))
+        device_config = import_module('modules.network.devices.{0}'.format(file))
+        devices[file] = device_config
+
+        # Load the scripts
+        scripts[file] = listdir(path.join('modules', 'network', 'devices', file))
+
+    config.DEVICES = devices
+    config.SCRIPTS = scripts
